8ddd66eda9dd043daedf2769ddbe44e46bfe780b
josippreva <josip.previsic@fsre.sum.ba>
2026-01-08 15:53:53 +0100

    Registracija

--- app/src/main/java/com/example/dnevnikraspolozenja/api/SupabaseAPI.java
+++ app/src/main/java/com/example/dnevnikraspolozenja/api/SupabaseAPI.java
@@ -1,5 +1,6 @@
 package com.example.dnevnikraspolozenja.api;
 
+import com.example.dnevnikraspolozenja.models.request.RegisterRequest;
 
 import com.example.dnevnikraspolozenja.models.request.LoginRequest;
 import com.example.dnevnikraspolozenja.models.response.AuthResponse;
@@ -13,4 +14,10 @@ public interface SupabaseAPI {
     @Headers("Content-Type: application/json")
     @POST("auth/v1/token?grant_type=password")
     Call<AuthResponse> login(@Body LoginRequest request);
+
+
+
+
+    @POST("auth/v1/signup")
+    Call<AuthResponse> signup(@Body RegisterRequest request);
 }
---
+++ app/src/main/java/com/example/dnevnikraspolozenja/models/request/RegisterRequest.java
@@ -0,0 +1,11 @@
+package com.example.dnevnikraspolozenja.models.request;
+
+public class RegisterRequest {
+    private String email;
+    private String password;
+
+    public RegisterRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
